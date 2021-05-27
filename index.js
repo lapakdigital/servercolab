23dab98ef4119e3ef6fa735526ff060c105b3524
lapakdigital <37134646+lapakdigital@users.noreply.github.com>
2021-05-27 20:42:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(bp.urlencoded({
     extended: true
 }));
 
+app.get('/', async (req, res) => {
+		res.send('tes')
+})
+
 // app.post('/upload/', async (req, res) => {
 //     // console.log(req.body)
 //     // const data = req.params.data
